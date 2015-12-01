7b03eb877593ad21cdd6f277e931a4b9bea642b9
zhaoqing <qing.zhao@cgtz.com>
2015-12-01 16:55:32 +0800

    zq

--- protected/services/v0/SmsService.php
+++ protected/services/v0/SmsService.php
@@ -39,6 +39,10 @@ class SmsService extends AppApiService
         $str='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
         $randStr = str_shuffle($str);//打乱字符串
         $rands= substr($randStr,0,6);//substr(string,start,length);返回字符串的一部分
+
+        //++++++测试start+++++//
+        $rands = '654321';
+        //++++++测试end+++++++//
         return $rands;
     }
 }
