210d8b13032e9be5e2893deb4dec41bdcedcb4b9
zhaoqing <qing.zhao@cgtz.com>
2015-12-03 16:08:41 +0800

    master

--- protected/services/v0/SmsService.php
+++ protected/services/v0/SmsService.php
@@ -19,9 +19,10 @@ class SmsService extends AppApiService
             $cache = Yii::app()->cache;
             $cache->hset($mobile,$type,$num);
 
-            $sms = new Sms();
-            $result = $sms->send($mobile, '【Urtime】您的注册验证码是：'.$num.'.请完成注册',true);
-            $result = $sms->execResult($result);
+            //$sms = new Sms();
+            $result = $this->send($mobile, '【Urtime】您的注册验证码是：'.$num.'.请完成注册',true);
+            //$result = $sms->execResult($result);
+
             if($result[1]==0){
                // echo '发送成功';
                 $ret = $this->notice('OK', 0, '', $result);
@@ -47,6 +48,24 @@ class SmsService extends AppApiService
         return $rands;
     }
 
+    public function send($mobile,$msg,$needstatus = 'false',$product = '',$extno = ''){
+        $postArr = array(
+            'account'       =>'jiekou-clcs-08',//Yii::app()->params['chuanglan']['api_account'],
+            'pswd'          =>'Txb654321',//Yii::app()->params['chuanglan']['api_password'],
+            'msg'           =>$msg,
+            'mobile'        => $mobile,
+            //'needstatus'    =>$needstatus,
+            // 'product'       =>$product,
+            //'extno'         =>$extno,
+        );
+        //$snoopy = new Snoopy();
+        // $result = $snoopy->submit(Yii::app()->params['chuanglan']['api_send_url'],$postArr);
+        //$result = $snoopy->submitlinks(Yii::app()->params['chuanglan']['api_send_url'],$postArr);
+        //$result = $this->curlPost("http://222.73.117.158/msg/HttpBatchSendSM",$postArr);
+        $result = $this->doGet("http://222.73.117.158/msg/HttpBatchSendSM",$postArr);
+        return $result;
+    }
+
     /**
      * 请求远端post提取返回数据
      * $url 远端请求地址
@@ -64,6 +83,36 @@ class SmsService extends AppApiService
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         if($json!=false) curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: '.strlen($data)));
+        $result = curl_exec($curl);
+        curl_close($curl);
+        return $result;
+    }
+
+    /**
+     * 请求远端get提取返回数据
+     * $url 远端请求地址
+     * $data 需要get的数据以数组方式
+     */
+    public function doGet($url,$data=false){
+        if(is_array($data)){
+            foreach($data AS $k=>$v){
+                $grr[]=$k.'='.$v;
+            }
+            $txt = implode($grr,'&');
+        }else{
+            $txt = $data;
+        }
+        $target = $txt ? $url.'?'.$txt : $url;
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $target); // 要访问的地址
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1); // 从证书中检查SSL加密算法是否存在
+        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
+        //curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
+        //curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+        curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
         $result = curl_exec($curl);
         curl_close($curl);
         return $result;
