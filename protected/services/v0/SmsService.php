29bbc33da5c1174f0ab8e49deae8701468658c7e
zhaoqing <qing.zhao@cgtz.com>
2015-12-03 16:56:02 +0800

    zq

--- protected/services/v0/SmsService.php
+++ protected/services/v0/SmsService.php
@@ -58,11 +58,11 @@ class SmsService extends AppApiService
             // 'product'       =>$product,
             //'extno'         =>$extno,
         );
-        //$snoopy = new Snoopy();
-        // $result = $snoopy->submit(Yii::app()->params['chuanglan']['api_send_url'],$postArr);
+       //$snoopy = new Snoopy();
+       //  $result = $snoopy->submit(Yii::app()->params['chuanglan']['api_send_url'],$postArr);
         //$result = $snoopy->submitlinks(Yii::app()->params['chuanglan']['api_send_url'],$postArr);
         //$result = $this->curlPost("http://222.73.117.158/msg/HttpBatchSendSM",$postArr);
-        $result = $this->doGet("http://222.73.117.158/msg/HttpBatchSendSM",$postArr);
+        $result = $this->doPost("http://222.73.117.158/msg/HttpBatchSendSM",$postArr);
         return $result;
     }
 
@@ -74,9 +74,6 @@ class SmsService extends AppApiService
     public function doPost($url,$data=array(),$json=false){
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        //curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-        //curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1);
-        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
         curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
         curl_setopt($curl, CURLOPT_TIMEOUT, 30);
@@ -105,13 +102,6 @@ class SmsService extends AppApiService
         $target = $txt ? $url.'?'.$txt : $url;
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $target); // 要访问的地址
-       // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
-        //curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1); // 从证书中检查SSL加密算法是否存在
-        //curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
-        //curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
-        //curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
-        //curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
-        //curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
         $result = curl_exec($curl);
         curl_close($curl);
