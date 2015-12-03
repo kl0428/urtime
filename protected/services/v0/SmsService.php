7eee59d03d8e7506040c33046949b2a7dc119481
zhaoqing <qing.zhao@cgtz.com>
2015-12-03 16:20:31 +0800

    master

--- protected/services/v0/SmsService.php
+++ protected/services/v0/SmsService.php
@@ -54,7 +54,7 @@ class SmsService extends AppApiService
             'pswd'          =>'Txb654321',//Yii::app()->params['chuanglan']['api_password'],
             'msg'           =>$msg,
             'mobile'        => $mobile,
-            //'needstatus'    =>$needstatus,
+            'needstatus'    =>$needstatus,
             // 'product'       =>$product,
             //'extno'         =>$extno,
         );
@@ -105,13 +105,13 @@ class SmsService extends AppApiService
         $target = $txt ? $url.'?'.$txt : $url;
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $target); // 要访问的地址
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1); // 从证书中检查SSL加密算法是否存在
-        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
+       // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+        //curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1); // 从证书中检查SSL加密算法是否存在
+        //curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
         //curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
         //curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
-        curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
-        curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+        //curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+        //curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
         $result = curl_exec($curl);
         curl_close($curl);
