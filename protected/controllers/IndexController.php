8113573760c5df7338c5dde3655b8aa9d1b9c132
zhaoqing <qing.zhao@cgtz.com>
2015-12-18 11:17:11 +0800

    zq

--- protected/controllers/IndexController.php
+++ protected/controllers/IndexController.php
@@ -287,4 +287,9 @@ class IndexController extends Controller
 
     }
 
+    public function actionUrtime()
+    {
+        $this->render("urtime");
+    }
+
 }
---
+++ protected/views/index/urtime.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="ch">
+<head>
+    <?php Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl.'/css/style0728.css');?>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+    <meta charset="utf-8" />
+    <meta HTTP-EQUIV="pragma" CONTENT="no-cache">
+    <meta HTTP-EQUIV="Cache-Control" CONTENT="no-cache, must-revalidate">
+    <meta HTTP-EQUIV="expires" CONTENT="0">
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="apple-touch-fullscreen" content="yes" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta http-equiv="Expires" content="-1" />
+    <meta http-equiv="pragram" content="no-cache" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <?php Yii::app()->clientScript->registerCoreScript('jquery');?>
+</head>
+<style>
+    h4 { padding-top: 30px; padding-top: 0.9375rem; line-height: 45px; line-height: 1.40625rem; font-size: 30px; font-size: 0.9375rem;}
+    p { font-size: 26px; font-size: 0.8125rem; line-height: 45px; line-height: 1.40625rem; text-indent: 2em; text-align: justify;}
+    .container { background-color: #fff;}
+    .head { margin: 0 20px; margin: 0 0.625rem; padding-bottom: 20px; padding-bottom: 0.625rem; border-bottom: 1px solid #ddd; text-align: center;}
+    .head p { text-indent: 0; text-align: center;}
+    .content { padding: 0 20px; padding: 0 0.625rem; background-color: #fff; }
+    .content p em { color: #fa5c5b;}
+    .content .ch { line-height: 50px; line-height: 1.5625rem; text-indent: 0;}
+    .content a { display: block; padding: 20px 0; padding: 0.625rem 0; line-height: 45px; line-height: 1.40625rem; text-indent: 2em; color: #e70014; font-size: 26px; font-size: 0.8125rem;}
+    .content .warn { padding: 20px 0; padding: 0.625rem 0; color: #e70014}
+    .content .fontRed { color: #fc101c;}
+    .content .fontBlod { font-weight: bold;}
+    .content .foot { position: static; padding-bottom: 40px; padding-bottom: 1.25rem; text-align: right;}
+    .content .noIndent { text-indent: 0;}
+</style>
+<body class="app">
+<div class="container">
+    <div class="head">
+        <h4>200亿大冲关之全场加息</h4>
+        <p>2015.11.30</p>
+    </div>
+    <div class="content">
+        <p class="ch">尊敬的各位草根会员：</p>
+        <p>
+            草根投资200亿大冲关，好消息接二连三。据说福利要双重才更吸引人，返现和加息在一起才更配！大声宣布：我们还有加息！对，全！场！不需要盯紧时间开抢，就静静的在官网等着你。草粉们，high起来~~
+        </p>
+        <p><em>12月1日0时起</em>，新上线项目：项目周期＜180天，加息<em class="fontBlod">0.5%</em> ；项目周期≥180天，加息<em class="fontBlod">1%</em>。</p>
+        <p><em>注：草根钱包、转让项目、新手项目、私人定制不参与加息。</em></p>
+        <p class="noIndent fontBlod">活动结束时间以官网公告为准。</p>
+        <p class="noIndent">让我们一起不断变的更好，让投资实现梦想。</p>
+        <p class="noIndent">本次公告最终解释权归浙江草根网络科技有限公司所有。</p>
+        <p class="foot">
+            草根团队<br/>
+            2015年11月30日
+        </p>
+    </div>
+</div>
+</body>
